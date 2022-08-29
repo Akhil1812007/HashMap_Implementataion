ea1b49ddcda5d683c4b3ff8d4f2858552baeb124
Akhil1812007 <akhil.bindhu@outlook.com>
2022-08-29 14:26:05 +0530

    COMMIT 1

--- HashMap_Implementataion/Map.cs
+++ HashMap_Implementataion/Map.cs
@@ -15,27 +15,35 @@ namespace HashMap_Implementataion
         public Map()
         {
             buckets = new List<MapNode<K,V>>();
-            numBuckets = 20;
+            numBuckets = 5;
             for (int i = 0; i < numBuckets; i++)
             {
                 buckets.Add(null);
             }
         }
+        public double loadFactor()
+        {
+            return (1.0 * count) / numBuckets;
+        }
         private int getBucketIndex(K Key)//GETTING THE HASHCODE TO DECIDE THE INDEX OF THE ELEMENT
         {
             int hc = Key.GetHashCode();
-            int index = hc % numBuckets;
-            if(index < 0)
+            int index = hc%numBuckets;
+            if (index < 0)
             {
-                index = -1*index;
+                index = -1 * index;
             }
+            //--------------------Debbuging--------------
+            //Console.WriteLine("get Bucket Index for  " + Key + " is " + index);
+            //Console.WriteLine();
+
             return index;
         }
         public void insert(K Key, V Value)
         {
             int bucketIndex = getBucketIndex(Key);
-            MapNode<K, V> head = buckets.ElementAt(bucketIndex);
-            //element is not there ? just update its value
+            MapNode<K,V> head = buckets.ElementAt(bucketIndex);
+            //element is  there  just update its value
             while (head!= null)
             {
                 if (head.key.Equals(Key))
@@ -47,11 +55,17 @@ namespace HashMap_Implementataion
             }
             // element is not there.insert at 0th position of linked list
             head = buckets.ElementAt(bucketIndex);
-            MapNode<K,V> newNode = new MapNode<K,V>(Key, Value);
-            newNode.next = head;
-            buckets.Insert(bucketIndex, newNode);
+            MapNode<K,V> newNode = new MapNode<K,V>(Key,Value);
             count++;
-            double LoadingFactor = (1.0 * count) / numBuckets;
+            newNode.next = head;
+            buckets.Insert(bucketIndex,newNode);
+            //-------------------------------DEBUGGING----------------------
+            //Console.WriteLine("insert "+newNode.key +" value "+newNode.value+ "Bucket Index is "+bucketIndex);//DEBUGGING
+            //Console.WriteLine();    
+
+            //Console.WriteLine("key "+newNode.key +"value "+newNode.value);//DEBUGGING
+
+            double LoadingFactor = (1.0*count)/numBuckets;
             if (LoadingFactor > 0.75)
             {
                 reHash();
@@ -61,9 +75,12 @@ namespace HashMap_Implementataion
         {
             return count;
         }
-        public V getValue(K Key)
+        public V getValue(K Key)// TO GET THE VALUE BASED ON A PARTICULAR KEY
         {
             int bucketIndex = getBucketIndex(Key);
+            //-----------------DEBUGGING
+            //Console.WriteLine("BucketIndex for "+Key+" is "+bucketIndex);
+            //Console.WriteLine();
             MapNode<K, V> head = buckets.ElementAt(bucketIndex);
             //element is not there ? just update its value
             while (head != null)
@@ -71,6 +88,10 @@ namespace HashMap_Implementataion
                 if (head.key.Equals(Key))
                 {
                     return head.value;
+                    //---------------DEBUGGING----------
+                    //Console.WriteLine("Element "+ head.key + " " + head.value);
+                    //Console.WriteLine( " Key " + head.key + " value " + head.value+ "  BucketIndex at get value" + bucketIndex );//DEBUGGING
+                    //Console.WriteLine();
                 }
                 head = head.next;
             }
@@ -82,6 +103,7 @@ namespace HashMap_Implementataion
         {
             int bucketIndex = getBucketIndex(Key);
             MapNode<K, V> head = buckets.ElementAt(bucketIndex);
+
             MapNode<K, V> prev = null;
             while (head != null)
             {
@@ -109,22 +131,26 @@ namespace HashMap_Implementataion
         //REHASHING FOR MAINTAINING THE LOADING FACTOR  < 0.75
         private void reHash()
         {
-            List<MapNode<K, V>> temp = buckets;
+            List<MapNode<K,V>> temp = buckets;
+            //Console.WriteLine("count = "+temp.Count );---debugging
+            
             buckets = new List<MapNode<K,V>>();
-            for (int i = 0; i < 2 * numBuckets; i++)
+            for (int i = 0; i < 2*numBuckets; i++)
             {
                 buckets.Add(null);
             }
             count = 0;
             numBuckets = numBuckets * 2;
-            for (int i = 0; i < temp.Count(); i++)
+            for (int i = 0; i <temp.Count; i++)
             {
                 MapNode<K, V> head = temp.ElementAt(i);
+                
                 while (head != null)
                 {
+                   // Console.WriteLine("Element " + i + " " + head.key + " " + head.value);
                     K Key = head.key;
                     V value = head.value;
-                    insert(Key, value);
+                    insert(Key,value);
                     head = head.next;
                 }
             }
--- HashMap_Implementataion/Program.cs
+++ HashMap_Implementataion/Program.cs
@@ -7,16 +7,18 @@ namespace HashMap_Implementataion
         public static void Main(string[] args)
         {
             Map<String, int> map = new Map<String,int>();
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
             {
-                map.insert("abc" + i, i + 1);
+                map.insert("KEY"+i,i+1);
+               
                 
             }
            
-            for (int i = 0; i < 20; i++)
+            for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine("abc" + i + ":" + map.getValue("abc" + i));
+                Console.WriteLine("KEY " + i + ":" + map.getValue("KEY"+i));
             }
+            Console.WriteLine(map.size());
         }
     }
 }
